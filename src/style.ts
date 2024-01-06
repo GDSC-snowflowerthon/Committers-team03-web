398e6e148d115a99d1cf1681c4d9f0ef075b9d1d
minseong0324 <kwan0324@naver.com>
2024-01-07 05:42:37 +0900

    Fix : font issue

--- src/style.ts
+++ src/style.ts
@@ -1,39 +1,31 @@
 import {createGlobalStyle} from 'styled-components';
-import NanumBarunpenB from './static/font/NanumBarunpenB.woff2';
-import BareunHipi from './static/font/BareunHipi.woff2';
+import Pretendard from './static/font/Pretendard-Medium.woff2';
 
 export const GlobalStyle = createGlobalStyle`
-    @font-face {
-        font-family: 'NanumBarunpenB';
-        src: url(${NanumBarunpenB}) format('woff2');
-        font-weight: normal;
-        font-style: normal;
-    }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url(${Pretendard}) format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
 
-    @font-face {
-        font-family: 'BareunHipi';
-        src: url(${BareunHipi}) format('woff2');
-        font-weight: normal;
-        font-style: normal;
-    }
+  html {
+    font-size: 16px; // 16px 기본 설정
+    //background-color:  #8a6149;
+    overflow-x: hidden;
+  }
 
-    html {
-        font-size: 16px; // 16px 기본 설정
-        overflow-x: hidden;
-    }
+  body, html {
+    font-family: 'Pretendard';
+    font-weight: 700;
+    margin: 0;
+    font-size: 16px;
+  }
 
-    body, html {
-        font-family: 'NanumBarunpenB';
-        font-weight: 700;
-        margin: 0;
-        font-size: 16px;
-        background-color: #2B2A4C;
-    }
-
-    #content {
-        position: relative;
-        height: 100%;
-        overflow: auto;
-        z-index: 1;
-    }
+  #content {
+    position: relative;
+    height: 100%;
+    overflow: auto;
+    z-index: 1;
+  }
 `;
