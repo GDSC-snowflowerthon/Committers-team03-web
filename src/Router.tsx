987963742e541b3445ad8a578b85dab0f0f473f7
minseong0324 <kwan0324@naver.com>
2024-01-07 12:39:06 +0900

    Feat : Login page routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,11 +4,13 @@ import Custom from './pages/Custom/Custom';
 import Ranking from './pages/Ranking/Ranking';
 import Follow from './pages/Follow/Follow';
 import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 export default function Router() {
 
   return (
     <>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/:nickname" element={<Home />} />
         <Route path="/custom" element={<Custom />} />
         <Route path="/ranking" element={<Ranking />} />
