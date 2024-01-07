24d076ce6d4b001efd291bd247d61736ff2cb019
minseong0324 <kwan0324@naver.com>
2024-01-08 08:48:18 +0900

    Feat: 메인 페이지는 페이지 최하단으로

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -1,12 +1,20 @@
-import {useEffect} from 'react';
-import {useLocation} from 'react-router-dom';
-
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function ScrollToTop() {
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    // 페이지 최상단으로 스크롤을 이동시킬 경로 목록
+    const topScrollRoutes = ['/', '/custom', '/ranking', '/follow', '/register'];
+
+    if (topScrollRoutes.includes(pathname)) {
+      // 지정된 경로 중 하나일 경우, 페이지 최상단으로 스크롤
+      window.scrollTo(0, 0);
+    } else {
+      // 그 외의 경우, 페이지 최하단으로 스크롤
+      window.scrollTo(0, document.body.scrollHeight);
+    }
   }, [pathname]);
 
   return null;
