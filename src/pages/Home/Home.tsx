533168eeef5a48c76bc34289bc7a687c2e1bf784
minseong0324 <kwan0324@naver.com>
2024-01-08 08:52:33 +0900

    Fix : 눈사람 position 조정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MainPageLayout from '@/components/MainPageLayout/MainPageLayout'
 import * as S from './style'
 import SnowFalling from '@/components/SnowFalling/SnowFalling'
 import SnowmanScene from '@/components/SnowmanScene/SnowmanScene';
-import { useRecoilValue } from 'recoil';
-import { snowmanHeightState } from '@/atoms/snowmanState';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { snowmanHeightState, snowmanYPositionState } from '@/atoms/snowmanState';
 
 export default function Home() {
   const snowmanHeight = useRecoilValue(snowmanHeightState);
-
+  const [, setYPosition] = useRecoilState(snowmanYPositionState);
+  // 컴포넌트가 마운트될 때 yPosition 상태를 업데이트
+  useEffect(() => {
+    setYPosition(-1.6); // 원하는 초기 yPosition 값으로 설정
+  }, [setYPosition]);
+  
   return (
 
     <MainPageLayout >
