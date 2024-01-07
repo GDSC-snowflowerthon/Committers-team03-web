934a2e24fc6ea0b000558e51628de700407f0fd9
minseong0324 <kwan0324@naver.com>
2024-01-08 01:28:15 +0900

    Fix : snowfalling style 수정

--- src/components/SnowFalling/style.ts
+++ src/components/SnowFalling/style.ts
@@ -58,13 +58,13 @@ export const SnowWrapper = styled.div`
   width: 100%;
   max-width: 820px;
   max-height: 1180px;
-  height: 800px;
+  height: 870px;
   top: -30px;
   left: 50%;
   right: 50%;
   transform: translateX(-50%);
   overflow: hidden;
-  //z-index: 1;
+  z-index: 0;
 `;
 
 export const Container = styled.div`
