08035da86cdb975bc6b5bb47af9d64398ef024dd
minseong0324 <kwan0324@naver.com>
2024-01-08 10:34:30 +0900

    Chore : 불필요 주석 제거

--- src/hooks/useBackgroundTheme.ts
+++ src/hooks/useBackgroundTheme.ts
@@ -3,22 +3,19 @@ import { snowmanHeightState } from '@/atoms/snowmanState';
 
 export const useBackgroundTheme = () => {
     const snowmanHeight = useRecoilValue(snowmanHeightState);
-  
+
     const calculateGradient = (height: number): string => {
-      // 높이에 따른 그라데이션 비율 계산
-      const gradientStart = Math.min(100, (height - 250) / 150 * 100);
-      const gradientEnd = Math.min(100, gradientStart + 50);
-  
-      if (height < 250) {
-        return '#90C9F6'; // 기본 색상
-      } else if (height >= 250 && height < 400) {
-        // 그라데이션 적용
-        return `linear-gradient(to bottom, #2E44BC ${gradientStart}%, #90C9F6 ${gradientEnd}%)`;
-      } else {
-        return '#2E44BC'; // 네이비색
-      }
+        const gradientStart = Math.min(100, (height - 250) / 150 * 100);
+        const gradientEnd = Math.min(100, gradientStart + 50);
+    
+        if (height < 250) {
+            return '#90C9F6'; // 기본 색상
+        } else if (height >= 250 && height < 400) {
+            return `linear-gradient(to bottom, #2E44BC ${gradientStart}%, #90C9F6 ${gradientEnd}%)`;
+        } else {
+            return '#2E44BC';
+        }
     };
-  
+
     return calculateGradient(snowmanHeight);
-  };
-  
+};
