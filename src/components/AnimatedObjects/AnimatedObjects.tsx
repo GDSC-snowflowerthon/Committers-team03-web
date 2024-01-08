c3ed37585107ba96632da843dc3cfea4bfca9caf
minseong0324 <kwan0324@naver.com>
2024-01-09 01:00:34 +0900

    Fix : 오브젝트 애니메이션 컴포넌트 타입 이슈

--- src/components/AnimatedObjects/AnimatedObjects.tsx
+++ src/components/AnimatedObjects/AnimatedObjects.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { AnimatedObjectWrapper } from './style'; // 스타일 파일의 경로를 확인하세요
 
-export default function AnimatedObjects({ backgroundImageUrl }) {
+interface AnimatedObjectsProps {
+  backgroundImageUrl: string;
+}
+
+export default function AnimatedObjects({ backgroundImageUrl }: AnimatedObjectsProps) {
   return (
-    <AnimatedObjectWrapper background={backgroundImageUrl} />
+    <AnimatedObjectWrapper backgroundImageUrl={backgroundImageUrl} />
   );
 }
