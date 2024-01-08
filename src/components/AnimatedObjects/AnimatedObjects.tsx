8fe17d7f0a589ae71132727311307bb7ccf18e38
minseong0324 <kwan0324@naver.com>
2024-01-09 00:38:55 +0900

    Feat : 오브젝트 애니메이션 추가

--- src/components/AnimatedObjects/AnimatedObjects.tsx
+++ src/components/AnimatedObjects/AnimatedObjects.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { AnimatedObjectWrapper } from './style'; // 스타일 파일의 경로를 확인하세요
 
-export default function AnimatedObjects() {
+export default function AnimatedObjects({ backgroundImageUrl }) {
   return (
-    <div>AnimatedObjects</div>
-  )
+    <AnimatedObjectWrapper background={backgroundImageUrl} />
+  );
 }
--- src/components/AnimatedObjects/style.ts
+++ src/components/AnimatedObjects/style.ts
@@ -1,43 +1,22 @@
-import { css, keyframes, styled } from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-
-interface ButtonWrapperProps {
-    background: string; // 배경 이미지 URL을 받을 수 있도록 설정
-    // ... 다른 스타일 관련 props
-  }
-  
-// 오른쪽 화살표 애니메이션
+// 위아래로 움직이는 애니메이션
 const moveUpDown = keyframes`
   0% { transform: translateY(0); }
-  50% { transform: translateY(5px); }
+  50% { transform: translateY(-10px); }
   100% { transform: translateY(0); }
 `;
 
-export const AnimatedObjectWrapper = styled.div<ButtonWrapperProps>`
-    position: absolute;
-    top: 50%;
-    right: 5px;
-    z-index: 0;
-    background-color: transparent;
-    background-size: 200px 200px;
-    background-repeat: no-repeat;
-    width: 200px;
-    height: 200px;
-        
-    ${(props) => 
-        props.background == RightArrowButtonImg &&
-        css`
-        /* LeftArrowButtonImg에 대한 스타일 */
-        right: 15px;
-        z-index: 5;
-        animation: ${moveUpDown} 0.5s ease-in-out infinite;
-        @media (min-width: 820px) {
-            position: fixed;
-            left: calc(50% + 350px);
-        }
-
-        background: url(${RightArrowButtonImg}); /* 배경 이미지 설정 */
-        `
-    }
-
+export const AnimatedObjectWrapper = styled.div<{ background: string }>`
+  position: absolute;
+  top: 50%;
+  right: 5px;
+  z-index: 10;
+  background-image: url(${props => props.background});
+  background-color: transparent;
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 200px; // 이미지 크기에 맞게 조절
+  height: 200px; // 이미지 크기에 맞게 조절
+  animation: ${moveUpDown} 3s ease-in-out infinite; // 애니메이션 적용
 `;
