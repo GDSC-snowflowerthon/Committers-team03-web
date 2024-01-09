f3559b78c1540ec6cbcb187058b0f70119d56c63
minseong0324 <kwan0324@naver.com>
2024-01-10 08:31:38 +0900

    Feat: 이미지 추가 및 카드 변경

--- src/atoms/snowmanState.ts
+++ src/atoms/snowmanState.ts
@@ -12,5 +12,5 @@ export const snowmanRotationState = atom<boolean>({
 
 export const snowmanHeightState = atom<number>({
   key: 'snowmanHeightState',
-  default: 130, // 초기 눈사람의 키는 0으로 설정
+  default: 190, // 초기 눈사람의 키는 0으로 설정
 });
