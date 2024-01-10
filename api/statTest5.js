85a57d8b3ac5ea7aa694a719688eae369e73a9b1
minseong0324 <kwan0324@naver.com>
2024-01-11 03:25:29 +0900

    Fix : 이미지 최적화 및 구현 방식 변경

--- api/statTest5.js
+++ api/statTest5.js
@@ -11,32 +11,27 @@ export default async (req, res) => {
     // 파일 URL 결정
     const baseUrl = 'https://commiters-team03-web.vercel.app';
     const fileNumber = Math.min(Math.floor(snowmanHeight / 30), 10);
-    const cardUrl = `${baseUrl}/Card${fileNumber}.svg`;
-    const objectUrl = `${baseUrl}/Object${fileNumber}.svg`;
-    const snowmanUrl = `${baseUrl}/Snowman000.svg`;
-    const weatherUrl =
-      fileNumber >= 4 ? `${baseUrl}/Star.svg` : `${baseUrl}/Snow.svg`;
+    const urls = [
+      `${baseUrl}/Card${fileNumber}.svg`,
+      `${baseUrl}/Object${fileNumber}.svg`,
+      `${baseUrl}/Snowman000.svg`,
+      fileNumber >= 4 ? `${baseUrl}/Star.svg` : `${baseUrl}/Snow.svg`,
+    ];
 
-    // 각각의 SVG 파일을 가져옵니다.
-    const fetchSvg = async (url) => {
-      const response = await fetch(url);
+    // 모든 SVG 파일을 동시에 가져옵니다.
+    const svgResponses = await Promise.all(urls.map((url) => fetch(url)));
+    svgResponses.forEach((response) => {
       if (!response.ok)
         throw new Error(`Failed to fetch SVG: ${response.statusText}`);
-      return await response.text();
-    };
-
-    const cardSvg = await fetchSvg(cardUrl);
-    const objectSvg = await fetchSvg(objectUrl);
-    const snowmanSvg = await fetchSvg(snowmanUrl);
-    const weatherSvg = await fetchSvg(weatherUrl);
+    });
+    const svgs = await Promise.all(
+      svgResponses.map((response) => response.text()),
+    );
 
     // 모든 SVG를 조합합니다.
     const combinedSvg = `
       <svg width="400" height="200" xmlns="http://www.w3.org/2000/svg">
-        ${cardSvg}
-        ${objectSvg}
-        ${snowmanSvg}
-        ${weatherSvg}
+        ${svgs.join('')}
         <style>
           .header { font: 700 18px 'Segoe UI', Ubuntu, Sans-Serif; fill: #2f80ed; }
           .stat { font: 600 14px 'Segoe UI', Ubuntu, "Helvetica Neue", Sans-Serif; fill: #333 }
