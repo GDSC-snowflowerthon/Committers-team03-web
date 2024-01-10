bb0cbb579b653f6947da90800caff703cb53762d
minseong0324 <kwan0324@naver.com>
2024-01-10 11:15:07 +0900

    Fix : 이미지 인코딩 이슈

--- api/stat.js
+++ api/stat.js
@@ -11,6 +11,46 @@ import Card9 from '/9.svg';
 import Card10 from '/10.svg';
 
 export default async (req, res) => {
+  // snowmanHeight에 따라 파일 이름을 결정합니다.
+  const filePath = (data) => {
+    if (data.snowmanHeight <= 130) {
+      return Card0;
+    } else if (data.snowmanHeight <= 160) {
+      return Card1;
+    } else if (data.nowmanHeight <= 190) {
+      return Card2;
+    } else if (data.snowmanHeight <= 220) {
+      return Card3;
+    } else if (data.snowmanHeight <= 250) {
+      return Card4;
+    } else if (data.snowmanHeight <= 280) {
+      return Card5;
+    } else if (data.snowmanHeight <= 310) {
+      return Card6;
+    } else if (data.snowmanHeight <= 340) {
+      return Card7;
+    } else if (data.snowmanHeight <= 370) {
+      return Card8;
+    } else if (data.snowmanHeight <= 410) {
+      return Card9;
+    } else {
+      return Card10;
+    }
+  };
+
+  const renderSVG = (data) => {
+    return `
+    ${filePath}
+    <style>
+    .header { font: 700 18px 'Segoe UI', Ubuntu, Sans-Serif; fill: #2f80ed; }
+    .stat { font: 600 14px 'Segoe UI', Ubuntu, "Helvetica Neue", Sans-Serif; fill: #333 }
+    </style>
+    <text x="10" y="25" class="header">Snowman Heights for ${data.nickname}</text>
+    <text x="10" y="55" class="stat">Height: ${data.snowmanHeight} cm</text>
+    <text x="10" y="80" class="stat">Attacked: ${data.damage} times</text>
+  </svg>
+  `;
+  };
   try {
     // req.query에서 필요한 데이터를 직접 분해하여 할당합니다.
     const {
@@ -33,32 +73,6 @@ export default async (req, res) => {
       decoId,
     };
 
-    // snowmanHeight에 따라 파일 이름을 결정합니다.
-    let filename;
-    if (data.snowmanHeight <= 130) {
-      filename = Card0;
-    } else if (snowmanHeight <= 160) {
-      filename = Card1;
-    } else if (snowmanHeight <= 190) {
-      filename = Card2;
-    } else if (snowmanHeight <= 220) {
-      filename = Card3;
-    } else if (snowmanHeight <= 250) {
-      filename = Card4;
-    } else if (snowmanHeight <= 280) {
-      filename = Card5;
-    } else if (snowmanHeight <= 310) {
-      filename = Card6;
-    } else if (snowmanHeight <= 340) {
-      filename = Card7;
-    } else if (snowmanHeight <= 370) {
-      filename = Card8;
-    } else if (snowmanHeight <= 410) {
-      filename = Card9;
-    } else {
-      filename = Card10;
-    }
-
     // 파일 시스템 모듈을 사용하여 public 폴더에서 SVG 파일을 읽습니다.
     // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
     //const fs = require('fs');
@@ -66,17 +80,21 @@ export default async (req, res) => {
     //const path = require('path');
     // eslint-disable-next-line no-undef
     //const filePath = path.join(process.cwd(), 'public', filename);
-    console.log(`${filename}`);
+    //console.log(`${filename}`);
     // SVG 파일을 읽습니다.
-    let svg = `${filename}
-      <style>
-      .header { font: 700 18px 'Segoe UI', Ubuntu, Sans-Serif; fill: #2f80ed; }
-      .stat { font: 600 14px 'Segoe UI', Ubuntu, "Helvetica Neue", Sans-Serif; fill: #333 }
-      </style>
-      <text x="10" y="25" class="header">Snowman Heights for ${nickname}</text>
-      <text x="10" y="55" class="stat">Height: ${snowmanHeight} cm</text>
-      <text x="10" y="80" class="stat">Attacked: ${damage} times</text>
-    </svg>`;
+    // const renderSVG = () => {
+    //   return `
+    //   ${filename}
+    //   <style>
+    //   .header { font: 700 18px 'Segoe UI', Ubuntu, Sans-Serif; fill: #2f80ed; }
+    //   .stat { font: 600 14px 'Segoe UI', Ubuntu, "Helvetica Neue", Sans-Serif; fill: #333 }
+    //   </style>
+    //   <text x="10" y="25" class="header">Snowman Heights for ${data.nickname}</text>
+    //   <text x="10" y="55" class="stat">Height: ${data.snowmanHeight} cm</text>
+    //   <text x="10" y="80" class="stat">Attacked: ${data.damage} times</text>
+    // </svg>
+    // `;
+    // };
     // // SVG 내용에 텍스트 요소를 추가합니다.
     // (svg = `${filename}
     //   <style>
@@ -87,7 +105,7 @@ export default async (req, res) => {
     //   <text x="10" y="55" class="stat">Height: ${snowmanHeight} cm</text>
     //   <text x="10" y="80" class="stat">Attacked: ${damage} times</text>
     // </svg>`);
-
+    const svg = renderSVG(data);
     res.setHeader('Content-Type', 'image/svg+xml');
     res.status(200).send(svg);
   } catch (error) {
