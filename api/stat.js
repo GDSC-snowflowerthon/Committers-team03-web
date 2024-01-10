df53c67644fdad4cb89ae888d2af44be072839c0
minseong0324 <kwan0324@naver.com>
2024-01-10 10:16:37 +0900

    Fix: 이미지 경로 읽지 못함 이슈

--- api/stat.js
+++ api/stat.js
@@ -1,16 +1,14 @@
-// import Card0 from '../public/0.svg';
-// import Card1 from '../public/1.svg';
-// import Card2 from '../public/2.svg';
-// import Card3 from '../public/3.svg';
-// import Card4 from '../public/4.svg';
-// import Card5 from '../public/5.svg';
-// import Card6 from '../public/6.svg';
-// import Card7 from '../public/7.svg';
-// import Card8 from '../public/8.svg';
-// import Card9 from '../public/9.svg';
-// import Card10 from '../public/10.svg';
-import fs from 'fs';
-// import path from 'path';
+import Card0 from '/0.svg';
+import Card1 from '/1.svg';
+import Card2 from '/2.svg';
+import Card3 from '/3.svg';
+import Card4 from '/4.svg';
+import Card5 from '/5.svg';
+import Card6 from '/6.svg';
+import Card7 from '/7.svg';
+import Card8 from '/8.svg';
+import Card9 from '/9.svg';
+import Card10 from '/10.svg';
 
 export default async (req, res) => {
   try {
@@ -38,27 +36,27 @@ export default async (req, res) => {
     // snowmanHeight에 따라 파일 이름을 결정합니다.
     let filename;
     if (data.snowmanHeight <= 130) {
-      filename = 'https://commiters-team03-web.vercel.app/0.svg';
+      filename = Card0;
     } else if (snowmanHeight <= 160) {
-      filename = 'https://commiters-team03-web.vercel.app/1.svg';
+      filename = Card1;
     } else if (snowmanHeight <= 190) {
-      filename = 'https://commiters-team03-web.vercel.app/2.svg';
+      filename = Card2;
     } else if (snowmanHeight <= 220) {
-      filename = 'https://commiters-team03-web.vercel.app/3.svg';
+      filename = Card3;
     } else if (snowmanHeight <= 250) {
-      filename = 'https://commiters-team03-web.vercel.app/4.svg';
+      filename = Card4;
     } else if (snowmanHeight <= 280) {
-      filename = 'https://commiters-team03-web.vercel.app/5.svg';
+      filename = Card5;
     } else if (snowmanHeight <= 310) {
-      filename = 'https://commiters-team03-web.vercel.app/6.svg';
+      filename = Card6;
     } else if (snowmanHeight <= 340) {
-      filename = 'https://commiters-team03-web.vercel.app/7.svg';
+      filename = Card7;
     } else if (snowmanHeight <= 370) {
-      filename = 'https://commiters-team03-web.vercel.app/8.svg';
+      filename = Card8;
     } else if (snowmanHeight <= 410) {
-      filename = 'https://commiters-team03-web.vercel.app/9.svg';
+      filename = Card9;
     } else {
-      filename = 'https://commiters-team03-web.vercel.app/10.svg';
+      filename = Card10;
     }
 
     // 파일 시스템 모듈을 사용하여 public 폴더에서 SVG 파일을 읽습니다.
@@ -70,7 +68,7 @@ export default async (req, res) => {
     //const filePath = path.join(process.cwd(), 'public', filename);
     console.log(`${filename}`);
     // SVG 파일을 읽습니다.
-    let svg = await filename.text();
+    let svg = filename;
 
     // SVG 내용에 텍스트 요소를 추가합니다.
     svg = svg.replace(
