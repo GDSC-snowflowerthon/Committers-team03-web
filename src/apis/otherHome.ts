0e4409e0bcb5fb193713353b52bfee9f418282f5
minseong0324 <kwan0324@naver.com>
2024-01-12 01:38:32 +0900

    Feat: 자신인지 타인인지에 따른 분기처리

--- src/apis/otherHome.ts
+++ src/apis/otherHome.ts
@@ -1,5 +1,24 @@
 import {instance} from './axios';
 
+// 다른 유저 정보 조회
+export const getOtherData = async (nickname: string) => {
+  const response = await instance.get(`/api/v1/user?nickname=${nickname}`);
+  return {
+    ...response.data,
+  };
+}
+
+{/*
+	"data":{
+			"nickname" : "gitHubId", // 깃허브 아이디
+			"snowmanHeight" : 30, //눈사람 키
+			"snowId" : 3,
+			"hatId" : 5,
+			"decoId" : 7
+			"isFollowed" : false //2024-01-08 추가
+	}
+*/}
+
  // 친구 상태 변경 (팔로우/언팔로우)
   export const patchSnowmanGrow = async (nickname: string, isFollowed: boolean) => {
     try {
@@ -20,25 +39,6 @@ import {instance} from './axios';
 		"isFollowed" : "true" // 친구 추가 여부(true or false)
 	}
 }
-*/}
-
-// 다른 유저 정보 조회
-export const getOtherData = async (nickname: string) => {
-  const response = await instance.get(`/api/v1/user?nickname=${nickname}`);
-  return {
-    ...response.data,
-  };
-}
-
-{/*
-	"data":{
-			"nickname" : "gitHubId", // 깃허브 아이디
-			"snowmanHeight" : 30, //눈사람 키
-			"snowId" : 3,
-			"hatId" : 5,
-			"decoId" : 7
-			"isFollowed" : false //2024-01-08 추가
-	}
 */}
 
 // 공격하기
