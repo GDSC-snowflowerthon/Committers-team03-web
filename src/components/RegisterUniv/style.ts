d1d60e93e84c7d2ab6dd3f5f301d4d107cc4caf6
cywin1018 <cywin0821@gmail.com>
2024-01-12 11:11:14 +0900

    Chore : 학교등록 스타일 변경

--- src/components/RegisterUniv/style.ts
+++ src/components/RegisterUniv/style.ts
@@ -1,32 +1,32 @@
 
 import { styled } from "styled-components";
-
+import theme from '@/theme';
 export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
   background-color: #ffffff;
-  margin-right: 8px;
-  width: 380px;
+  width: 85%;
   height: 45px;
   padding: 8px;
   border: 1px solid #ccc;
-  border-radius: 10px;
+  border-radius: 12px;
   outline: none;
   margin-top: 2rem;
 `;
 
 export const Text = styled.div`
   font-size: 14px;
-  color: #4F5861;
+  color: ${theme.colors.textMain};
 
 `;
 
 export const RegisterButton = styled.button<{ clicked: boolean }>`
   width: 60px;
   height: 35px;
-  color: #ffffff;
-  border-radius: 4px;
+  color: ${theme.colors.white};
+  border-radius: 12px;
+  font-weight: 600;
   background-color: ${props => props.clicked ? '#7E9ECC' : '#3181F4'};
   background-size: cover;
   background-repeat: no-repeat;
