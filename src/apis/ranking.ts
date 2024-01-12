f13ab4aa44d808c31995c674feeb642609e22e6e
minseong0324 <kwan0324@naver.com>
2024-01-12 23:21:12 +0900

    Fix: api baseurl 삭제

--- src/apis/ranking.ts
+++ src/apis/ranking.ts
@@ -1,12 +1,11 @@
 import {instance} from './axios';
-const baseServerURL = 'https://kidari-server.shop';
 
 export const getBuddyRankingData = async () => {
-  const response = await instance.get(baseServerURL+`/api/v1/ranking/buddy/own`);
+  const response = await instance.get(`/api/v1/ranking/buddy/own`);
     return response.data;
 };
 
 export const getUnivRankingData = async () => {
-    const response = await instance.get(baseServerURL+`/api/v1/ranking/univ/own`);
+    const response = await instance.get(`/api/v1/ranking/univ/own`);
       return response.data;
   };
